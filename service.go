fa1c36d525538d546ea932a9c29abb19262ad6e4
Arpan KC <nipeshkc7@gmail.com>
2021-07-18 15:22:23 +1000

    starting a web server

--- service.go
+++ service.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Server is running!")
+	fmt.Println("Homepage called")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Starting Go Server !")
+	handleRequests()
 }
