c8311410c3539ac9376b23ea2949e8c0729b14fc
Arpan KC <nipeshkc7@gmail.com>
2021-07-18 15:23:36 +1000

    ping endpoint

--- service.go
+++ service.go
@@ -6,13 +6,13 @@ import (
 	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
+func ping(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Server is running!")
-	fmt.Println("Homepage called")
+	fmt.Println("pong")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
+	http.HandleFunc("/", ping)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
